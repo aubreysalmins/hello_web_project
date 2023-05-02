8712992f168e072cd76baa20e99862e4ff26de61
Aubrey Salmins <aubreysalmins@gmail.com>
2023-05-02 11:44:52 +0100

    Add step 2 challenge

--- app.rb
+++ app.rb
@@ -7,4 +7,18 @@ class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+
+  get '/hello' do
+    name = params[:name]
+
+    # puts params
+    return "Hello #{name}"
+  end
+
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+
+    return "Thanks #{name}, you sent this message: '#{message}'"
+  end
 end
